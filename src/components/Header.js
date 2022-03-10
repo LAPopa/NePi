f661a12adef53a06e34770cae3696782ddbd871c
LAPopa <laura_stanescu_1991@yahoo.com>
2022-03-10 10:19:55 +0200

    rectified (minor) header/footer behaviour

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,10 @@ import NePiLogo from '../assets/NePiplaceholder.png';
 
 export default function Header() {
     return (
-        <header className="text-gray-600 body-font">
+        <header className="text-gray-600 body-font bg-gray-100">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                    {/*<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"*/}
-                    {/*     stroke-linejoin="round" stroke-width="2"*/}
-                    {/*     className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">*/}
-                    {/*    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/>*/}
-                    {/*</svg>*/}
+
                     <img src={NePiLogo} height="100" width="100"/>
                     <span className="ml-3 text-xl">NePi</span>
                 </a>

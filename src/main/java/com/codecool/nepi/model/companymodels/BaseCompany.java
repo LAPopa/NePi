a3d96e19be81732417a8e36970fac197f5f742dc
LAPopa <laura_stanescu_1991@yahoo.com>
2022-03-28 14:01:28 +0300

    BaseCompany set as entity, converter snippet

--- src/main/java/com/codecool/nepi/model/companymodels/BaseCompany.java
+++ src/main/java/com/codecool/nepi/model/companymodels/BaseCompany.java
@@ -1,24 +1,40 @@
 package com.codecool.nepi.model.companymodels;
 
 import com.codecool.nepi.model.types.CompanyType;
+import com.codecool.nepi.utils.StringListConverter;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-@Getter @Setter
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class BaseCompany {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String companyName;
     private CompanyType companyType;
     private String description;
+    @Convert(converter = StringListConverter.class)
     private List<String> allocatedIds;
+
+    @ElementCollection
+    @CollectionTable(name = "id_availability",
+    joinColumns = {@JoinColumn(name ="company_id", referencedColumnName="id")})
+    @MapKeyColumn(name="allocated_id")
+    @Column(name="available?")
     private HashMap<String, Boolean> idAvailability;
 
 
-    public BaseCompany(String companyName,CompanyType companyType, String description) {
+    public BaseCompany(String companyName, CompanyType companyType, String description) {
         this.companyName = companyName;
         this.companyType = companyType;
         this.description = description;
@@ -26,12 +42,12 @@ public class BaseCompany {
         this.idAvailability = new HashMap<>();
     }
 
-    public void addId(String id){
+    public void addId(String id) {
         this.allocatedIds.add(id);
-        this.idAvailability.put(id,true);
+        this.idAvailability.put(id, true);
     }
 
-    public void assignId(String id){
+    public void assignId(String id) {
         this.idAvailability.put(id, false);
 
     }

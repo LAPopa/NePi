453cc1b74cd338aa7ff32c6e9969b81b8a53946e
LAPopa <laura_stanescu_1991@yahoo.com>
2022-04-11 10:40:37 +0300

    [mosaic prep] updated dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import tailwindConfig from './tailwind.config'
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+export default {
+  plugins: [tailwind(tailwindConfig), autoprefixer],
 }
